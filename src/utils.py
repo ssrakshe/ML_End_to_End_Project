180d01de98d53823359ec5043480046091367c57
ssrakshe <raksheshubham542@gmail.com>
2024-04-16 23:54:05 -0500

    Data pipeline trained

--- src/utils.py
+++ src/utils.py
@@ -1,47 +1,62 @@
 import os
 import sys
-import numpy as np
+
+import numpy as np 
 import pandas as pd
+import dill
+
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
-import dill
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import r2_score
 
 def save_object(file_path, obj):
     try:
-        dir_path=os.path.dirname(file_path)
+        dir_path = os.path.dirname(file_path)
 
-        os.makedirs(dir_path,exist_ok=True)
-        with open(file_path,"wb") as file_obj:
-            dill.dump(obj,file_obj)
-    
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+
     except Exception as e:
-        raise CustomException(e,sys)
-
-
-def evaluate_models(x_train,y_train,x_test,y_test,models,params):
+        raise CustomException(e, sys)
+    
+def evaluate_models(x_train, y_train,x_test,y_test,models,param):
     try:
-        report={}
+        report = {}
 
         for i in range(len(list(models))):
-            model=list(models.values())[i]
-            para=params[list(models.keys())[i]]
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
-            gs=GridSearchCV(model,para,cv=3)
+            gs = GridSearchCV(model,para,cv=3)
             gs.fit(x_train,y_train)
 
             model.set_params(**gs.best_params_)
             model.fit(x_train,y_train)
-            #model.fit(x_train,y_train)
 
-            y_train_pred=model.predict(x_train)
-            y_test_pred =model.predict(x_test)
-            train_model_score=r2_score(y_train,y_train_pred)
-            test_model_score=r2_score(y_test,y_test_pred)
+            #model.fit(X_train, y_train)  # Train model
 
-            report[list(models.keys())[i]]=test_model_score
+            y_train_pred = model.predict(x_train)
+
+            y_test_pred = model.predict(x_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
 
         return report
+
     except Exception as e:
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
